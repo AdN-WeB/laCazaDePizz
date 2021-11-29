8fa7e3df3b3ab43e05fe281078c9646906dc4ead
AdN-WeB <adrien.mervelay84@gmail.com>
2021-11-29 20:27:09 +0100

    ajout boissons

--- src/app/boissons/boissons.component.ts
+++ src/app/boissons/boissons.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Boisson } from '../shared/models/boisson.model';
 
 @Component({
   selector: 'app-boissons',
@@ -7,7 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BoissonsComponent implements OnInit {
 
-  constructor() { }
+  boissons:Boisson [];
+
+  constructor() { 
+    this.boissons =[
+      new Boisson("Soda","33cl",1.5),
+      new Boisson("Soda","1 Litre",3.5),
+      new Boisson("Eau","50cl",1),
+      new Boisson("Eau","1 Litre",1.5),
+      new Boisson("Bierre","",2),
+      new Boisson("Panaché","",1.8),
+      new Boisson("Vin rosé","75cl",8)
+    ]
+  }
 
   ngOnInit(): void {
   }
