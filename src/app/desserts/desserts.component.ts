3ae2a64aa372955d73448d73950209ca2fe131e5
AdN-WeB <adrien.mervelay84@gmail.com>
2021-11-29 20:40:43 +0100

    ajout dessert

--- src/app/desserts/desserts.component.ts
+++ src/app/desserts/desserts.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Dessert } from '../shared/models/dessert.model';
 
 @Component({
   selector: 'app-desserts',
@@ -7,7 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DessertsComponent implements OnInit {
 
-  constructor() { }
+  desserts: Dessert[];
+
+  constructor() { 
+    this.desserts=[
+      new Dessert("Panna cotta","Coulis fruits rouges, exotiques ou nutella",3.5),
+      new Dessert("Tiramisu","Nutella speculoos ou café",3.5),
+      new Dessert("Mousse au chocolat","",3.5)
+    ]
+  }
 
   ngOnInit(): void {
   }
---
+++ src/app/shared/models/dessert.model.ts
@@ -0,0 +1,12 @@
+export class Dessert{
+
+    title:string;
+    description:string;
+    price:number;
+
+    constructor(title:string,description:string,price:number){
+        this.title=title;
+        this.description=description;
+        this.price=price
+    }
+}
