b8e36728f0e13fe391e962253adfbe4fb85b81a8
AdN-WeB <adrien.mervelay84@gmail.com>
2021-11-29 19:44:33 +0100

    suggestions création

--- src/app/carte-pates/carte-pates.component.ts
+++ src/app/carte-pates/carte-pates.component.ts
@@ -14,7 +14,7 @@ export class CartePatesComponent implements OnInit {
     this.pates= [
       new Pate("Allison Parker","Tagliatelles façon carbonara",9.9),
       new Pate("Mercedes","Caramela Ricotta épinard,sauce gorgonzola et noix",10.9),
-      new Pate("Arthuro","Pâtes aux Chorizo et poivrons confits",10.9),
+      new Pate("Arthurito","Pâtes aux Chorizo et poivrons confits",10.9),
       new Pate("Monica","Linguini épinards,légumes confits,Pesto",10),
       new Pate("Ariadna","Agnolotti saumon,sauce saumon",11),
     
--- src/app/carte-suggestions/carte-suggestions.component.ts
+++ src/app/carte-suggestions/carte-suggestions.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Suggestion } from '../shared/models/suggestion.model';
 
 @Component({
   selector: 'app-carte-suggestions',
@@ -7,7 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CarteSuggestionsComponent implements OnInit {
 
-  constructor() { }
+  suggestions: Suggestion[];
+
+  constructor() {
+    this.suggestions= [
+      new Suggestion("M16","Cassolette de ravioles aux saumon", 12.5),
+      new Suggestion("Glock17","Cassolette de ravioles aux gorgonzola et noix",12),
+      new Suggestion("Pistolet berreta","Cassolette de ravioles aux cèpes",13),
+      new Suggestion("Mitrailleuse Browing","Lasagne à la bolognaise",9.5),
+      new Suggestion("Lance-Flame","Lasagne saumon et épinards", 11),
+      new Suggestion("Grenade", "Polpette à la tomate et tagliatelles",9.5)
+    ]
+   }
 
   ngOnInit(): void {
   }
