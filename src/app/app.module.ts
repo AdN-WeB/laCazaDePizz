c880a75120bba99818aafc5062d26c18141a4e45
AdN-WeB <adrien.mervelay84@gmail.com>
2021-11-30 23:57:28 +0100

    ajout form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -41,7 +42,8 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,19 +1,38 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.css']
+  styleUrls: ['./contact.component.css'],
 })
-export class ContactComponent implements OnInit {
+export class ContactComponent {
+  contactUs: any | undefined
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(public fb: FormBuilder, private router: Router) {
+    this.contactUs = this.fb.group({
+      nameForm: ['', [Validators.required, Validators.minLength(3)]],
+      emailForm: [
+        '',
+        [Validators.required, Validators.minLength(5), Validators.email],
+      ],
+      msgForm: ['', [Validators.required, Validators.maxLength(200), Validators.minLength(3)]],
+    });
   }
+  
 
-  onSubmit(){
-    
+  onsubmit( $event : Event) {
+    $event.preventDefault();
+    if(this.contactUs.valid){
+      console.log(this.contactUs.value)
+    }
+  };
+  clear() {
+    this.contactUs.controls.nameForm.setValue('');
+    this.contactUs.controls.emailForm.setValue('');
+    this.contactUs.controls.msgForm.setValue('');
   }
-
+  redirected() {
+    this.router.navigate(['/home']);
+  }
 }
